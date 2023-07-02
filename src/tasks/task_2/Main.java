93886fed34795fa26e8cd385879bd2d69875e8c3
Yaroslav82 <yargay230607@gmail.com>
2023-07-02 14:20:59 +0300

    Add solution for task 2

--- src/tasks/task_2/Main.java
+++ src/tasks/task_2/Main.java
@@ -1,4 +1,47 @@
 package tasks.task_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private final static String ONION = "onion";
+
+    public static void main(String[] args) {
+        print(deleteOnion("orange", "plum", "tomato", "onion", "grape", "onion"));
+    }
+
+    /**
+     * Deletes all {@code ONION} from {@code array}.
+     * <p>
+     * Then it returns a List of Strings without {@code ONION}.
+     *
+     * @param   array   array of {@code String}.
+     * @return a {@code List<String>} without elements which value equals {@code ONION}.
+     */
+    private static List<String> deleteOnion(String... array) {
+        List<String> result = new ArrayList<>();
+
+        for (String str : array) {
+            if (!ONION.equals(str)) {
+                result.add(str);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Prints a list of {@code String} into a column, with element numbering.
+     *
+     * @param   strings   {@code List} of {@code String}.
+     */
+    private static void print(List<String> strings) {
+        int count = 1;
+
+        for (String str : strings) {
+            System.out.println(count + ") " + str);
+            count++;
+        }
+    }
 }
